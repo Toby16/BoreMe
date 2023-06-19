4bd21bbe3e5d275fca30eaa197306149b0eea1a0
Toby16 <bolajibaasit14@gmail.com>
2023-06-19 13:23:09 -0400

    BoreMe game | Report: made required changes to login route

--- BoreMe_app/routes.py
+++ BoreMe_app/routes.py
@@ -1,7 +1,8 @@
 from BoreMe_app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from BoreMe_app.forms import LoginForm
-
+from flask_login import current_user, login_user
+from BoreMe_app.models import User
 
 # view funtion to welome page
 @app.route("/")
@@ -24,13 +25,16 @@ def home():
 # view function for user login
 @app.route("/login/", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home")) # will change this when needed
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Login requested for user - {}, remember_me={}".format(
-            form.username.data,
-            form.remember_me.data)
-        )
-        return redirect("/welcome")
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for("home")) # will change this when needed
     return render_template("login.html", title="login", form=form)
 
 # view function for user account registeration
