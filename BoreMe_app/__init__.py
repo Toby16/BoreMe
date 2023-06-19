c54bada213ca9908eb9f1c7e519659f624285948
Toby16 <bolajibaasit14@gmail.com>
2023-06-19 13:22:40 -0400

    BoreMe game | Report: included a few updates for user login, etc

--- BoreMe_app/__init__.py
+++ BoreMe_app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-app = Flask(__name__)
+from flask_login import LoginManager
+
+app = Flask(__name__) # flask object
 app.config.from_object(Config)
 
 db = SQLAlchemy(app) # database object
 migrate = Migrate(app, db) # migration object
+
+login = LoginManager(app) # object to manage user logged-in state
 
 from BoreMe_app import routes, models
