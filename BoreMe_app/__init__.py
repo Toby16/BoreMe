5e5fea0b46d89946d9d5ffb893cba030d4a82835
Toby16 <bolajibaasit14@gmail.com>
2023-06-18 21:48:19 -0400

    BoreMe game | Report: updated | included 'db' object for 'database', and 'migrate' object for 'database migration'

--- BoreMe_app/__init__.py
+++ BoreMe_app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
-
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
+app.config.from_object(Config)
 
-from BoreMe_app import routes
+db = SQLAlchemy(app) # database object
+migrate = Migrate(app, db) # migration object
+
+from BoreMe_app import routes, models
