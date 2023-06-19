17e1b8d4268a4ae8da2ff7a0e1ae24d17c483fed
Toby16 <bolajibaasit14@gmail.com>
2023-06-18 21:45:43 -0400

    BoreMe game | Report: included 'flask shell' functionality

--- BoreMe_.py
+++ BoreMe_.py
@@ -1,1 +1,6 @@
-from BoreMe_app import app
+from BoreMe_app import app, db
+from BoreMe_app.models import User
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"db": db, "User": User}
