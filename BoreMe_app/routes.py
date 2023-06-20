8355464464945e0a81741e31a2da2f0cfc4a28e5
Toby16 <bolajibaasit14@gmail.com>
2023-06-19 21:24:28 -0400

    BoreMe game | Report: made changes to 'registeration' endpoint/view-function for account registeration

--- BoreMe_app/routes.py
+++ BoreMe_app/routes.py
@@ -1,13 +1,16 @@
-from BoreMe_app import app
-from flask import render_template, flash, redirect, url_for
-from BoreMe_app.forms import LoginForm
-from flask_login import current_user, login_user
+from BoreMe_app import app, db
+from flask import render_template, flash, redirect, url_for, request
+from BoreMe_app.forms import LoginForm, RegisterationForm
+from flask_login import current_user, login_user, logout_user, login_required
 from BoreMe_app.models import User
+from werkzeug.urls import url_parse
 
 # view funtion to welome page
 @app.route("/")
 @app.route("/welcome/")
 def welcome():
+    if current_user.is_authenticated:
+        return redirect(url_for("home")) # will change this when needed
     return render_template("welcome.html")
 
 
@@ -16,10 +19,15 @@ def welcome():
 def tutorial():
     return render_template("tutorial.html")
 
+
 # view function for Home page
 @app.route("/home/")
+@login_required
 def home():
-    return render_template("home.html")
+    # this view function will display a box which display the availble and general rooms to join for the game
+    # with a frontend indicating a title 'Rooms', and then including the rooms for user to join to then start the game with other users
+    # for now, users can't have private rooms, will add that as an update some other time
+    return render_template("home.html", title='Home')
 
 
 # view function for user login
@@ -34,13 +42,34 @@ def login():
             flash("Invalid username or password")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for("home")) # will change this when needed
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("home")
+        return redirect(next_page) # will change this when needed
     return render_template("login.html", title="login", form=form)
 
+
+# view function for current_user logout
+@app.route("/logout/")
+def logout():
+    logout_user() # logs a user out. This will also clean the 'remember_me' cookie if it exists
+    return redirect(url_for("home"))
+
 # view function for user account registeration
-@app.route("/register/")
+@app.route("/register/", methods=["POST", "GET"])
 def register():
-    return render_template("register.html")
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = RegisterationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+                    email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash ("Congratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Registeration", form=form)
 
 # view function for user profile after login
 @app.route("/user_profile/<username>")
