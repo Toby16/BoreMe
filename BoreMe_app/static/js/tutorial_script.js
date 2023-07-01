6e9e41d6bd50af4747ff5ddfda6fea37af336b95
Toby16 <bolajibaasit14@gmail.com>
2023-07-01 11:49:38 -0400

    BoreMe game | Report: made adjustments

--- BoreMe_app/static/js/tutorial_script.js
+++ BoreMe_app/static/js/tutorial_script.js
@@ -6,3 +6,11 @@ document.getElementById("tutorialButton").addEventListener("click", function(){
 document.getElementById("loginButton").addEventListener("click", function() {
     window.location.href = "/login";
 });
+
+/* remove flashed message after 3 seconds */
+const flashMessage = document.getElementById("flash_message")
+
+setTimeout(function() {
+    flashMessage.style.display = "none";
+
+}, 10000);
