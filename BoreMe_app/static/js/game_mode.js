7a20d1d3714965231a02d09b7cfcd0b16b12f6c5
Toby16 <bolajibaasit14@gmail.com>
2023-07-01 11:52:57 -0400

    BoreMe game | Report: JS functionality for 'welcome.html' template

---
+++ BoreMe_app/static/js/game_mode.js
@@ -0,0 +1,4 @@
+/* for button 'Tutorial' with id '#tutorial_button' on welcome page to direct to "tutorial.html" */
+document.getElementById("player_vs_pc").addEventListener("click", function(){
+    window.location.href = "/start/game/vs_pc";
+});
