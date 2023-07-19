ae23530cf8fdd06410f408f149323c096c46be04
Toby16 <bolajibaasit14@gmail.com>
2023-07-19 05:12:13 -0400

    BoreMe game | Report: updated and made changes

--- BoreMe_app/static/js/player_vs_pc.js
+++ BoreMe_app/static/js/player_vs_pc.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
   $('#game_status_value').click(function () {
 
@@ -12,8 +11,8 @@ $(document).ready(function () {
         url: '/start/game/vs_pc',
         success: function (response) {
           // Update the game status with the generated random number
-          console.log("target number:", response.target_number) /* will remove later - for testing */
-          $('#game_status_value').text('Random number generated: ' + response.target_number);
+          console.log("target number ->", response.target_number) /* will remove later - for testing */
+          $('#game_status_value').text('Random number generated');
         },
         error: function (error) { 
           console.log('AJAX request error:', error);
@@ -24,30 +23,69 @@ $(document).ready(function () {
 });
 
 
-/*
 $(document).ready(function () {
   $('#player_vs_pc_form').submit(function (e) {
     e.preventDefault(); // Prevent default form submission behavior
 
+    // Create an object with the user input
+    var formData = {
+      user_input: $('#user_input').val()
+    };
+
+    $('#player_status_value').text("checking...");
+
     // Make an AJAX request to process the form data
-    $.ajax({
-      type: 'POST',
-      url: '/start/game/vs_pc',
-      data: $(this).serialize(), // Serialize the form data
-      success: function (response) {
-        // Update the player status with the response
-        console.log("player status:", response.player_status); // for testing
-        $('#player_status_value').text(response.player_status);
-      },
-      error: function (error) {
-        console.log('AJAX request error:', error);
-      }
-    });
+    setTimeout(function () {
+      $.ajax({
+        type: 'POST',
+        url: '/start/game/vs_pc/player',
+        data: JSON.stringify(formData), // Convert the object to JSON
+        contentType: 'application/json', // Set the content type to JSON
+        success: function (response) {
+          // Update the player status with the response
+          //console.log("player status:", response.player_status); /* for testing */
+          $('#player_status_value').text(response.player_status);
+          $("#player_score_value").text(response.player_score);
+          $("#pc_score_value").text(response.pc_score);
+          $("#game_status_value").text(response.game_status);
+          console.log("player guess:", formData.user_input)
+          //console.log("player_score:", response.player_score) // for testing
+          //console.log("pc_score:", response.pc_score) // for testing
+
+          // If player's guess is incorrect, make a request for the PC's guess
+          if (response.player_status !== "Correct") {
+            // Update the game status to "PC guessing..."
+            $('#pc_status_value').text('PC guessing...');
+
+            // Make an AJAX request to get the PC's guess
+            setTimeout(function(){
+              $.ajax({
+                type: "POST",
+                url: "/start/game/vs_pc/pc",
+                success: function (pcResponse) {
+                  // Update the game status with the PC's guess
+                  console.log("PC guess:", pcResponse.pc_input) /* for testing */
+                  //$('#game_status_value').text('PC guess: ' + pcResponse.pc_guess);
+                  $('#pc_status_value').text(pcResponse.pc_status);
+                  $('#pc_score_value').text(pcResponse.pc_score);
+                  $("#game_status_value").text(pcResponse.game_status);
+                },
+                error: function (error) {
+                  console.log('AJAX request error:', error);
+                }
+              })
+            }, 2000);
+          }
+        },
+        error: function (error) {
+          console.log('AJAX request error:', error);
+        }
+      });
+    }, 2000);
   });
 });
-*/
 
-
+/*
 $(document).ready(function () {
   $('#player_vs_pc_form').submit(function (e) {
     e.preventDefault(); // Prevent default form submission behavior
@@ -65,7 +103,7 @@ $(document).ready(function () {
       contentType: 'application/json', // Set the content type to JSON
       success: function (response) {
         // Update the player status with the response
-        console.log("player status:", response.player_status); /* for testing */
+        console.log("player status:", response.player_status); //for testing
         $('#player_status_value').text(response.player_status);
         $("#player_score_value").text(response.player_score)
         $("#pc_score_value").text(response.pc_score)
@@ -78,3 +116,4 @@ $(document).ready(function () {
     });
   });
 });
+*/
