89488d1f9e78cb6c8a3c2b291ce27b2cb2e91540
Toby16 <bolajibaasit14@gmail.com>
2023-07-19 05:10:34 -0400

    BoreMe game | Report: updated general chat button to direct to chat.html

--- BoreMe_app/static/js/game_mode.js
+++ BoreMe_app/static/js/game_mode.js
@@ -1,4 +1,14 @@
-/* for button 'Tutorial' with id '#tutorial_button' on welcome page to direct to "tutorial.html" */
+// for button 'Player vs Pc' with id '#player_vs_pc' on welcome page to direct to "player_vs_pc.html"
 document.getElementById("player_vs_pc").addEventListener("click", function(){
     window.location.href = "/start/game/vs_pc";
 });
+
+// for button 'Player vs Player' with id 'player_vs_player' on home page to direct to "player_vs_player.html"
+document.getElementById("chat_room").addEventListener("click", function(){
+    window.location.href = "/start/game/chat_room";
+})
+
+// for button 'Group vs Group' with id '#group_vs_group' on home page to direct to "group_vs_group.html"
+document.getElementById("player_vs_player").addEventListener("click", function(){
+    window.location.href = "/start/game/player_vs_player";
+})
