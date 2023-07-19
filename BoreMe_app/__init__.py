1187e77cf48c59e10e953a5a6a0319c279bf6f9d
Toby16 <bolajibaasit14@gmail.com>
2023-07-19 04:59:01 -0400

    BoreMe game | Report: included "Flask-SocketIO" configurations

--- BoreMe_app/__init__.py
+++ BoreMe_app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_socketio import SocketIO
 
 app = Flask(__name__) # flask object
 app.config.from_object(Config)
@@ -12,5 +13,6 @@ migrate = Migrate(app, db) # migration object
 
 login = LoginManager(app) # object to manage user logged-in state
 login.login_view = "login" # requiring users to login
+socketio = SocketIO(app)
 
 from BoreMe_app import routes, models
